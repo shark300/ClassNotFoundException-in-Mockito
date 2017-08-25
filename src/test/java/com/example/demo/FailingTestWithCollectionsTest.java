2ff814d2c64f978a0788e110720bd7047e938a4b
Attila Hajdu <v-ahajdu@hotels.com>
2017-08-25 17:57:05 +0200

    Cleanup app

--- src/test/java/com/example/demo/FailingTestWithCollectionsTest.java
+++ src/test/java/com/example/demo/FailingTestWithCollectionsTest.java
@@ -4,6 +4,7 @@ import static org.mockito.BDDMockito.given;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -22,6 +23,20 @@ public class FailingTestWithCollectionsTest {
     @Test
     public void test() {
         given(testCollectionSourceProvider.getCollection(new ArrayList<Integer>())).willReturn(new ArrayList<>());
+    }
+
+    @Test
+    public void test3() {
+        List<Integer> collection = testCollectionSourceProvider.getCollection(new ArrayList<>());
+        given(collection).willReturn(new ArrayList<>());
+    }
+
+    @Test
+    public void test2() {
+        List<Integer> input = new ArrayList<>();
+        List<Integer> output = new ArrayList<>();
+        List<Integer> collection = testCollectionSourceProvider.getCollection(input);
+        given(collection).willReturn(output);
     }
 
     static class TestCollectionSourceProvider {
