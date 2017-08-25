e5ee44f78f0fa606679874d192c8bc89533ecbe9
Attila Hajdu <v-ahajdu@hotels.com>
2017-08-25 13:50:55 +0200

    Empty repository

---
+++ src/test/java/com/example/demo/FailingTestWithCollectionsTest.java
@@ -0,0 +1,32 @@
+package com.example.demo;
+
+import static org.mockito.BDDMockito.given;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+public class FailingTestWithCollectionsTest {
+    @Mock
+    private TestCollectionSourceProvider testCollectionSourceProvider;
+
+    @Before
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    public void test() {
+        given(testCollectionSourceProvider.getCollection(new ArrayList<>(), Long.class)).willReturn(new ArrayList<>());
+    }
+
+    static class TestCollectionSourceProvider {
+        <T extends Collection<E>, E> T getCollection(T collection, Class<E> elementType) {
+            return collection;
+        }
+    }
+}
